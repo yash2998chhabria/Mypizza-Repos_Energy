a0879be07ee2b69be7bfb8d4cd3be9e9b5f2bf4d
Yash Chhabria <yash2998chhabria@gmail.com>
2021-03-01 11:13:27 +0530

    Update urls.py

--- mypizzaplace/pizza_api/urls.py
+++ mypizzaplace/pizza_api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path 
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
 
 app_name = 'pizza_api'
 
+router.register('', views.PizzaList, basename='pizzacrud')
+
 urlpatterns = [
-    path('', views.PizzaList.as_view(), name = 'listcreate'),
-    # path('<int:pk>/', views.PizzaDetail.as_view(), name='detailcreate' )
+    #     path('', views.PizzaList.as_view(), name = 'listcreate'),
+    #     path('<int:pk>/', views.PizzaDetail.as_view(), name='detailcreate' )
 ]
+urlpatterns += router.urls
